9dd067edbedb98283c3251782f12caddb0e3ec40
Cote411 <apc51104@gmail.com>
2021-04-14 11:12:55 -0400

    edits 4

--- Long Jump/ListOfAthletes.java
+++ Long Jump/ListOfAthletes.java
@@ -3,7 +3,6 @@ import java.util.LinkedList;
 public class ListOfAthletes {
     public static void setList(){
         LinkedList<DataList> allInfo = new LinkedList<DataList>();
-        LinkedList<DataList> linky = new LinkedList<DataList>();
         
     }
 }
