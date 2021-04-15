8f919734f2ac372209c3b9916170589b413f0943
Cote411 <apc51104@gmail.com>
2021-04-15 12:27:20 -0400

    edit

--- Employee Inheritence/Hourly.java
+++ Employee Inheritence/Hourly.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Hourly extends Employee{
+    Hourly(){
+        Scanner myScan = new Scanner(System.in);
+        System.out.print("Please enter name. First nad Last: ");
+        String str = myScan.nextLine();
+        System.out.print("PLease enter hourly wage: ");
+        int hourlyWage = myScan.nextInt();
+        
+        System.out.print("Name: " + str + "\n Wage: " + hourlyWage);
+        
+    }
     
+
 }
--- Employee Inheritence/Main.java
+++ Employee Inheritence/Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(){
-        
+        Employee newWorker = new Employee();
+        newWorker.setEmployee();
     }
 }
