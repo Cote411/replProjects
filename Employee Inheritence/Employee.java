935f3dc4c7a69ee1a70b5400c408692796e0b881
Cote411 <apc51104@gmail.com>
2021-04-15 12:21:52 -0400

    edits

--- Employee Inheritence/Employee.java
+++ Employee Inheritence/Employee.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Employee{
-    Employee(){
+    void setEmployee(){
+        Scanner myScan = new Scanner(System.in);
+
         System.out.print("Press 1 for hourly, 2 for salary, and 3 for comission. ");
+        int inputNumber = myScan.nextInt();
+        if(inputNumber == 1){
+            Hourly hourlyWorker = new Hourly();
+        }else if(inputNumber == 2){
+            Salary SalaryWorker = new Salary();
+        }else if(inputNumber == 3){
+            Comission ComissionWorker = new Comission();
+        }else{
+            System.out.println("Error. PLease enter a valid number.");
+            setEmployee();
+        }
     }
 }
    
